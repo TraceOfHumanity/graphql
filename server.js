ee6278276e4c5b2ed1ae75d5f019e108fcd57c52
TraceOfHumanity <azalexxx02@gmail.com>
2023-07-21 20:18:32 +0300

    chapter 4

--- server.js
+++ server.js
@@ -10,8 +10,53 @@ const schema = buildSchema(`
         other: Int
         rollDice(numDice: Int!, numSides: Int): [Int]
         quoteOfTheDay: String
+        user(name: String = "John Test"): User
+        getDie(numSides: Int): RandomDie
+        getMessage: String
+    }
+    type User { 
+        name: String
+        age: Float
+    }
+    type RandomDie {
+        roll(numRolls: Int!): [Int]
+        numSides: Int
+        rollOnce: Int
+    }
+
+    type Mutation {
+        setMessage(message: String): String
     }
 `);
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+    }
+    age() {
+        return Math.floor(Math.random() * 100);
+    }
+}
+
+class RandomDie {
+    constructor(numSides) {
+        this.numSides = numSides;
+    }
+    rollOnce() {
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+    roll({ numRolls }) {
+        const output = [];
+        for (let i = 0; i < numRolls; i++) {
+            output.push(this.rollOnce());
+        }
+        return output;
+    }
+}
+
+const fakeDatabase = {
+
+};
 
 const rootValue = {
     hello: () => 'Привіт, світе!',
@@ -24,20 +69,15 @@ const rootValue = {
         return output;
     },
     quoteOfTheDay: () => Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within',
+    user: (args) => new User(args.name),
+    getDie: ({ numSides }) => new RandomDie(numSides || 6),
+    setMessage: ({ message }) => {
+        fakeDatabase.message = message;
+        return message;
+    },
+    getMessage: () => fakeDatabase.message,
 }
 
 app.use('/graphql', graphqlHTTP({ schema, rootValue, graphiql: true }));
 
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
-
-// graphql({
-//     schema,
-//     rootValue,
-//     source: '{ hello other}'
-// })
-//     .then((response) => {
-//         console.log(response);
-//     })
-//     .catch((error) => {
-//         console.error(error);
-//     });
